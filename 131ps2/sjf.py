fea914ccc485c084db1dff8f27826fdfcb6a439d
Pito Salas <pitosalas@users.noreply.github.com>
2023-10-18 14:50:09 -0400

    checkpoint

--- 131ps2/sjf.py
+++ 131ps2/sjf.py
@@ -1,3 +1,6 @@
+LOGGING = True
+
+
 class SJF:
     def __init__(self, filename):
         self.filename = filename
@@ -6,17 +9,20 @@ class SJF:
         self.timeline = []
         self.running = None
 
+    def log(self, message):
+        if LOGGING:
+            print(message)
 
 # The file is a csv with 6 columns, corresponding to:
 # wait, burst1, io1, burst2, io2, burst3
 # The local is a list of lists, indexed by process number, and within that indexed by tick
-  
+
     def generate_local_timelime(self):
         with open(self.filename, 'r') as f:
             for line in f:
                 process_lt = self.process_lt(line)
                 self.local_timeline.append(process_lt)
-    
+
     def process_lt(self, line):
         process_lt = []
         line_split = line.split(',')
@@ -34,55 +40,81 @@ class SJF:
     # those entries are a list of two items, the first is the process status and the second is
     # the local tick offset for that process. This method creates the entry for tick 0
     def initialize_time_line(self):
-        for process in self.local_timeline:
-            self.timeline.append([[process[0][0], 0]])
+        self.log("*** begin of first time init***")
+        first_entry = []
+        self.log(f"Local timeline:{self.local_timeline}")
+        for index, process in enumerate(self.local_timeline):
+            first_entry.append([process[0], 0])
+            self.log(first_entry)
+        self.timeline.append(first_entry)
+        self.log("*** end of first time init***")
 
     def propose_next_tick(self):
-        self.tick+=1
-        tick = self.tick # just for convenience
+        self.tick += 1
+        tick = self.tick  # just for convenience
         # first calculate it without regard to the fact that only one process can run at a time
-        for index, process in enumerate(self.local_timeline):
-            if process[tick] == 'c':
-                self.timeline[index].append(['c', None])
-            elif process[tick] == 'i':
-                self.timeline[index][tick].append(['i', None])
+        new_entry = []
+        for index, local_state in enumerate(self.local_timeline):
+            self.log(f"Propose next process: {index} {local_state}")
+            if local_state[tick] == 'c':
+                new_entry.append(['p', 0])
+            elif local_state[tick] == 'i':
+                new_entry.append(['i', 0])
             else:
-                self.timeline[index][tick].append(['-', None])
+                new_entry.append(['-', 0])
+        self.log(f"Proposed next tick:{new_entry}\n")
+        self.timeline.append(new_entry)
 
     def determine_run_text_tick(self):
         if self.running is not None and self.timeline[self.running][self.tick][0] == 'c':
-            print(f'process {self.running} continues running\n')
+            self.log(f'process {self.running} continues running\n')
             self.update_local_tick_offsets(self.running)
         else:
             try_running = None
-            estimated_size = 1
+            estimated_size = -1
             for index, process in enumerate(self.timeline):
-                if process[self.tick][0] == 'c':
+                self.log(f"{index} {process}")
+                if process[self.tick][0] == 'p':
                     if try_running is None:
                         try_running = index
                     elif self.job_size(index) < estimated_size:
                         try_running = index
                         estimated_size = self.job_size(index)
-    
+            if try_running is not None:
+                print(f'process {try_running} begins running\n')
+                self.running = try_running
+                self.timeline[try_running][self.tick][0] = 'c'
+            self.update_local_tick_offsets(try_running)
+
     def job_size(self, index):
         return 1
-    
+
     # for all processes other than skip_process, we take the local tick offset from the previous tick
     # and add one to it. For the skip_process, we just copy the local tuck offset.
     def update_local_tick_offsets(self, skip_process):
+        print(f"Skip Process {skip_process}")
         for index, process in enumerate(self.local_timeline):
+            print(
+                f"Process:{index} state:{process[self.tick]}")
             if process[self.tick][0] == 'r' and index != skip_process:
                 self.local_timeline[index][self.tick][1] = self.local_timeline[index][self.tick-1][1] + 1
             elif process[self.tick][0] == 'r' and index == skip_process:
-               self.local_timeline[index][self.tick][1] = self.local_timeline[index][self.tick-1][1]
+                self.local_timeline[index][self.tick][1] = self.local_timeline[index][self.tick-1][1]
             else:
                 print(":error in update_local tick offsets")
-            
-if __name__ == '__main__':  
+
+    def pretty_print_timeline(self):
+        for index, entry in enumerate(self.timeline):
+            print(f'tick:{index}: {entry}\n')
+
+
+if __name__ == '__main__':
     sjf = SJF('data0.csv')
     sjf.generate_local_timelime()
     sjf.initialize_time_line()
     sjf.propose_next_tick()
     sjf.determine_run_text_tick()
-    print(sjf.timeline)
-    
+    sjf.pretty_print_timeline()
+    sjf.propose_next_tick()
+    sjf.determine_run_text_tick()
+    sjf.pretty_print_timeline()
